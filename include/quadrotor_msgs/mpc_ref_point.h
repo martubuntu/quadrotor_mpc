9737f90c4745e7c2608ea34a8645d34f687a664b
martubuntu <martubuntu@gmail.com>
2026-08-17 22:16:53 +0800

    feat: make uav_mpc completely self-contained with built-in message generation

---
+++ include/quadrotor_msgs/mpc_ref_point.h
@@ -0,0 +1,13 @@
+#ifndef QUADROTOR_MSGS_MPC_REF_POINT_H
+#define QUADROTOR_MSGS_MPC_REF_POINT_H
+
+#include "uav_mpc/mpc_ref_point.h"
+
+namespace quadrotor_msgs
+{
+    typedef uav_mpc::mpc_ref_point mpc_ref_point;
+    typedef uav_mpc::mpc_ref_pointPtr mpc_ref_pointPtr;
+    typedef uav_mpc::mpc_ref_pointConstPtr mpc_ref_pointConstPtr;
+}
+
+#endif
---
+++ include/quadrotor_msgs/mpc_ref_traj.h
@@ -0,0 +1,13 @@
+#ifndef QUADROTOR_MSGS_MPC_REF_TRAJ_H
+#define QUADROTOR_MSGS_MPC_REF_TRAJ_H
+
+#include "uav_mpc/mpc_ref_traj.h"
+
+namespace quadrotor_msgs
+{
+    typedef uav_mpc::mpc_ref_traj mpc_ref_traj;
+    typedef uav_mpc::mpc_ref_trajPtr mpc_ref_trajPtr;
+    typedef uav_mpc::mpc_ref_trajConstPtr mpc_ref_trajConstPtr;
+}
+
+#endif
